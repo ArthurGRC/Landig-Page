295154e32c632760d9b0b8025ea65238d2d9629f
Arthur <arthurzinugf@gmail.com>
2022-05-15 22:33:35 -0300

    Segundo Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", function () {
+  document.querySelector("body").style.backgroundColor = "#DCDCDC";
+});
+
+console.log("oi");
